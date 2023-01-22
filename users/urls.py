1b45bbfb42282753ad6a72c30768c9f70a55c750
Sulyk <sylvain.boudacher@gmail.com>
2023-01-22 15:34:48 +0100

    add register page + change native href for django redirection

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path('login/', auth_views.LoginView.as_view(template_name='../templates/registration/login.html'), name='login'),
-    
+    path('register/', auth_views.LoginView.as_view(template_name='../templates/registration/register.html'), name='register'),
 ]
