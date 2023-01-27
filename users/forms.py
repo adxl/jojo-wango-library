c6590f869e27095888818c1efeaac55dc5625db5
Sulyk <sylvain.boudacher@gmail.com>
2023-01-27 18:21:38 +0100

    j'y arriver pô

--- users/forms.py
+++ users/forms.py
@@ -7,6 +7,7 @@ class UserRegisterForm(UserCreationForm):
     
     labelAttr = "block mb-2 text-sm font-medium text-gray-900 dark:text-white"
     inputAttr = "bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+    selectAttr = "bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
     
     first_name = forms.CharField(required=True, label='First Name')
     last_name = forms.CharField(required=True, label='Last Name')
@@ -14,6 +15,7 @@ class UserRegisterForm(UserCreationForm):
     password1 = forms.CharField(required=True, label='Password', widget=forms.PasswordInput)
     password2 = forms.CharField(required=True, label='Confirm Password', widget=forms.PasswordInput)
     username = forms.CharField(required=True, label='Username')
+    role = forms.ChoiceField(choices=[('no', 'No'), ('yes', 'Yes')], label='Are you a librairy?')
     
     
     first_name.widget.attrs.update({'class': inputAttr})
@@ -22,6 +24,7 @@ class UserRegisterForm(UserCreationForm):
     password1.widget.attrs.update({'class': inputAttr})
     password2.widget.attrs.update({'class': inputAttr})
     username.widget.attrs.update({'class': inputAttr})
+    role.widget.attrs.update({'class': selectAttr})
     
     
     class Meta(UserCreationForm.Meta):
--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
+
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+  user = models.OneToOneField(User, related_name="profile" , on_delete=models.CASCADE)
+  role = models.CharField(max_length=20, default="user")
